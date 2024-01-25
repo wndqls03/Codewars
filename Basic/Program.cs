13de9a66bdcc6bcbc5b716f0f6b69a014125d8b7
wndqls03 <113694275+wndqls03@users.noreply.github.com>
2024-01-25 13:30:27 +1300

    Add files via upload

---
+++ Basic/Program.cs
@@ -0,0 +1,15 @@
+﻿public static class Kata
+{
+    public static int Solution(int value)
+    {
+        int res = 0;
+        for (int i = 0; i < value; i++)
+        {
+            if (i % 3 == 0 || i % 5 == 0)
+            {
+                res += i;
+            }
+        }
+        return res;
+    }
+}
